0bce51aa54eec5cdce602ffe6ae18c82d2a751ad
Raimon Esteve <raimon@nan-tic.com>
2019-12-10 11:40:03 +0100

    Set intercompany user in the transaction

--- tests/test_company_account_sync.py
+++ tests/test_company_account_sync.py
@@ -40,9 +40,21 @@ class CompanyAccountSyncTestCase(ModuleTestCase):
         pool = Pool()
         Account = pool.get('account.account')
         AccountTemplate = pool.get('account.account.template')
+        User = pool.get('res.user')
 
         # Create Company
         main_company = create_company()
+
+        main_company_user = User()
+        main_company_user.name = 'Main Company'
+        main_company_user.login = 'main'
+        main_company_user.main_company = main_company
+        main_company_user.company = main_company
+        main_company_user.save()
+
+        main_company.intercompany_user = main_company_user
+        main_company.save()
+
         with set_company(main_company):
             create_chart(main_company)
 
@@ -55,6 +67,17 @@ class CompanyAccountSyncTestCase(ModuleTestCase):
         company1 = create_company(name='Dunder Mifflin First Branch')
         company1.parent = main_company
         company1.save()
+
+        company1_user = User()
+        company1_user.name = 'Company 1'
+        company1_user.login = 'company1'
+        company1_user.main_company = company1
+        company1_user.company = company1
+        company1_user.save()
+
+        company1.intercompany_user = company1_user
+        company1.save()
+
         with set_company(company1):
             create_chart(company1)
 
@@ -71,6 +94,17 @@ class CompanyAccountSyncTestCase(ModuleTestCase):
         company2 = create_company(name='Dunder Mifflin Second Branch')
         company2.parent = main_company
         company2.save()
+
+        company2_user = User()
+        company2_user.name = 'Company 2'
+        company2_user.login = 'company2'
+        company2_user.main_company = company2
+        company2_user.company = company2
+        company2_user.save()
+
+        company2.intercompany_user = company2_user
+        company2.save()
+
         with set_company(company2):
             create_chart(company2)
 
